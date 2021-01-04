8771ec46579813be5dbc0f269e4780cb1000d3ac
letitgobaby <jinwoo4687@gmail.com>
2021-01-04 09:03:46 +0900

    디펜던시 정리

--- client/src/main/java/com/example/client/ClientApplication.java
+++ client/src/main/java/com/example/client/ClientApplication.java
@@ -4,13 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+
 @ComponentScan(basePackages = {"com.example"})
 @SpringBootApplication
 public class ClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientApplication.class, args);
-		
 		System.out.println("This is Client !!");
 	}
 
