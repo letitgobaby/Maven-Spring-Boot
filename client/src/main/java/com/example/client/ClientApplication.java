08436d86d7305c110a1c7a5e410fc2a59d810fed
letitgobaby <jinwoo4687@gmail.com>
2021-01-07 10:52:04 +0900

    [client] 로컬에서 파일 불러오는 경로 설정

--- client/src/main/java/com/example/client/ClientApplication.java
+++ client/src/main/java/com/example/client/ClientApplication.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
-@ComponentScan(basePackages = {"com.example"})
+@ComponentScan(basePackages = {"com.example", "com.core"})
 @SpringBootApplication
 @EnableJpaRepositories
 public class ClientApplication {
