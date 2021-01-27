c36fc1ae6558afe712fa36afe47961e882067b2e
letitgobaby <48303144+letitgobaby@users.noreply.github.com>
2021-01-27 18:07:58 +0900

    [core] 코드 정리

--- core/src/main/java/com/example/core/security/JwtTokenProvider.java
+++ core/src/main/java/com/example/core/security/JwtTokenProvider.java
@@ -35,46 +35,43 @@ public class JwtTokenProvider {
     secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
   }
 
-    // JWT 토큰 생성
-    public String createToken(String userPk, List<String> roles) {
-      Claims claims = Jwts.claims().setSubject(userPk); // JWT payload 에 저장되는 정보단위
-      claims.put("roles", roles); // 정보는 key / value 쌍으로 저장된다.
-      Date now = new Date();
-      
-      return Jwts.builder()
-              .setClaims(claims) // 정보 저장
-              .setIssuedAt(now) // 토큰 발행 시간 정보
-              .setExpiration(new Date(now.getTime() + tokenValidTime)) // set Expire Time
-              .signWith(SignatureAlgorithm.HS256, secretKey)  // 사용할 암호화 알고리즘과 
-                                                              // signature 에 들어갈 secret값 세팅
-              .compact();
+  // JWT 토큰 생성
+  public String createToken(String userPk, List<String> roles) {
+    Claims claims = Jwts.claims().setSubject(userPk); // JWT payload 에 저장되는 정보단위
+    claims.put("roles", roles); // 정보는 key / value 쌍으로 저장된다.
+    Date now = new Date();
+
+    return Jwts.builder().setClaims(claims) // 정보 저장
+        .setIssuedAt(now) // 토큰 발행 시간 정보
+        .setExpiration(new Date(now.getTime() + tokenValidTime)) // set Expire Time
+        .signWith(SignatureAlgorithm.HS256, secretKey) // 사용할 암호화 알고리즘과
+        .compact();  // signature 에 들어갈 secret값 세팅
   }
 
   // JWT 토큰에서 인증 정보 조회
   public Authentication getAuthentication(String token) {
-      UserDetails userDetails = userDetailsService.loadUserByUsername(this.getUserPk(token));
-      return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    UserDetails userDetails = userDetailsService.loadUserByUsername(this.getUserPk(token));
+    return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
   }
 
   // 토큰에서 회원 정보 추출
   public String getUserPk(String token) {
-      return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+    return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
   }
 
   // Request의 Header에서 token 값을 가져옵니다. "X-AUTH-TOKEN" : "TOKEN값'
   public String resolveToken(HttpServletRequest request) {
-      return request.getHeader("X-AUTH-TOKEN");
+    return request.getHeader("X-AUTH-TOKEN");
   }
 
   // 토큰의 유효성 + 만료일자 확인
   public boolean validateToken(String jwtToken) {
-      try {
-          Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken);
-          return !claims.getBody().getExpiration().before(new Date());
-      } catch (Exception e) {
-          return false;
-      }
+    try {
+      Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken);
+      return !claims.getBody().getExpiration().before(new Date());
+    } catch (Exception e) {
+      return false;
+    }
   }
-
 
 }
